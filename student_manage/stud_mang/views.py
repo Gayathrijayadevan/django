9a8da89c4eb0f1ff7f97d6820360a44d43427149
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-04 11:30:48 +0530

    stud_mang

--- student_manage/stud_mang/views.py
+++ student_manage/stud_mang/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+std=[]
+
+def home(req):
+    if req.method=='POST':
+        roll=req.POST['rollno']
+        name=req.POST['name']
+        age=req.POST['age']
+        std.append({'rollno':roll,'name':name,'age':age})
+        return redirect(home)
+    else:
+        return render(req,'home.html',{'students':std})
