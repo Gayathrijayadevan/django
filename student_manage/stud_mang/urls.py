cf9d315e640def4468936faa830183ba5a7bf5e6
Gayathrijayadevan <gayathrijayadevan11@gmail.com>
2024-11-05 11:35:48 +0530

    models

--- student_manage/stud_mang/urls.py
+++ student_manage/stud_mang/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path('home',views.home)
+    path('home',views.home),
+    path('edit/<a>',views.Edit_std),
+    path('delete/<a>',views.Delete),
 ]
--- student_manage/stud_mang/views.py
+++ student_manage/stud_mang/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
-std=[]
+std=[{'rollno':'1','name':'anu','age':'21'},{'rollno':'2','name':'anita','age':'22'},{'rollno':'3','name':'harry','age':'22'}]
 
 def home(req):
     if req.method=='POST':
@@ -12,3 +12,27 @@ def home(req):
         return redirect(home)
     else:
         return render(req,'home.html',{'students':std})
+    
+def Edit_std(req,a):
+    data=''
+    for i in std:
+            if i['rollno']==a:
+                    data=i
+    if req.method=='POST'    :
+            roll=req.POST['rollno']
+            name=req.POST['name']
+            age=req.POST['age']
+
+            data['rollno']=roll
+            data['name']=name
+            data['age']=age
+            return redirect(home)
+
+    else:
+            return render(req,'edit.html',{'data':data})
+    
+def Delete(req,a):
+    for i in std:
+            if i['rollno']==a:
+                std.remove(i)
+    return redirect(home)          
